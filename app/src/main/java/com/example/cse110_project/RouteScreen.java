0e25902c6551e7113993693eefcf237231066cbb
Howard Lin <houan.howard@gmail.com>
2020-02-08 01:07:22 -0800

    first load form done

--- app/src/main/java/com/example/cse110_project/RouteScreen.java
+++ app/src/main/java/com/example/cse110_project/RouteScreen.java
@@ -1,14 +1,48 @@
 package com.example.cse110_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RouteScreen extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.route_screen);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return false;
+            }
+        });
     }
+
+    private void selectFragment(MenuItem item){
+
+        Intent newIntent = new Intent(this, this.getClass());
+        switch(item.getItemId()) {
+            case R.id.navigation_home:
+                newIntent = new Intent(this, HomeScreen.class);
+                startActivity(newIntent);
+                break;
+            case R.id.navigation_walk:
+                newIntent = new Intent(this, WalkScreen.class);
+                startActivity(newIntent);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/cse110_project/StrideCalculator.java
@@ -0,0 +1,24 @@
+package com.example.cse110_project;
+
+public class StrideCalculator {
+
+    private int ft;
+    private int inch;
+    private final int FT_IN_INCH = 12;
+    private final double CONSTANT = 0.413;
+
+    public StrideCalculator(int ft, int inch){
+        this.ft = ft;
+        this.inch = inch;
+    }
+
+    public double getStrideLength(){
+        // Multiply height in inches by 0.413 then convert your stride length back to feet
+        // 63 inches * 0.413 = 26.019 inches / stride = 2.16825 feet / stride
+        return (this.ft * FT_IN_INCH + this.inch) * CONSTANT / FT_IN_INCH;
+    }
+
+    public void setFt(int newFt) { this.ft = newFt; }
+    public void setInch(int newInch) { this.inch = newInch; }
+
+}
