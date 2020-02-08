21d9f53ff9b937fd389c00e608537197f46913fc
Howard Lin <houan.howard@gmail.com>
2020-02-07 23:43:32 -0800

    added walk screen ui

--- app/src/main/java/com/example/cse110_project/HomeScreen.java
+++ app/src/main/java/com/example/cse110_project/HomeScreen.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +18,8 @@ public class HomeScreen extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private Button startWalkBtn;
     private Button addRouteBtn;
+    private Chronometer mChronometer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +35,34 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
 
+
+        mChronometer = findViewById(R.id.homeTimer);
+        mChronometer.setVisibility(View.INVISIBLE);
+
         startWalkBtn = (Button) findViewById(R.id.startBtn);
+
         startWalkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this, WalkScreen.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                launchThing();
+                System.out.println("BEFORE CHECK");
+                if(getIntent().hasExtra("START_TIME")) {
+                    long base = getIntent().getLongExtra("START_TIME", 0L);
+                    startTimer(base);
+
+                }
+                System.out.println("AFTER CHECK");
             }
         });
+
+//        startWalkBtn = (Button) findViewById(R.id.startBtn);
+//        startWalkBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(HomeScreen.this, WalkScreen.class);
+//                startActivity(intent);
+//            }
+//        });
 
         addRouteBtn = (Button) findViewById(R.id.addBtn);
         addRouteBtn.setOnClickListener(new View.OnClickListener() {
@@ -69,5 +93,32 @@ public class HomeScreen extends AppCompatActivity {
             default:
                 break;
         }
+    }
+
+    public void launchThing() {
+        Intent intent = new Intent(this, WalkScreen.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    public void startTimer(long base) {
+        System.out.println("STARTING TIMER");
+        mChronometer.setVisibility(View.VISIBLE);
+        mChronometer.setBase(base);
+        mChronometer.start();
+        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long time = SystemClock.elapsedRealtime() - chronometer.getBase();
+                int h = (int) (time / 3600000);
+                int m = (int) (time - h * 3600000) / 60000;
+                int s = (int) (time - h * 3600000 - m * 60000) / 1000;
+                String hStr = h < 10 ? "0" + h : h + "";
+                String mStr = m < 10 ? "0" + m : m + "";
+                String sStr = s < 10 ? "0" + s : s + "";
+                String format = hStr + ":" + mStr + ":" + sStr;
+                chronometer.setText(format);
+            }
+        });
     }
 }
