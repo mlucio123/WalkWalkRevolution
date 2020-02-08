a37f19b5f80ce14a23ac0b14531728c832d1073d
Howard Lin <houan.howard@gmail.com>
2020-02-07 18:35:14 -0800

    Added switcher and home screen

--- app/src/main/java/com/example/cse110_project/HomeScreen.java
+++ app/src/main/java/com/example/cse110_project/HomeScreen.java
@@ -1,14 +1,73 @@
 package com.example.cse110_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Button startWalkBtn;
+    private Button addRouteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return false;
+            }
+        });
+
+        startWalkBtn = (Button) findViewById(R.id.startBtn);
+        startWalkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreen.this, WalkScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        addRouteBtn = (Button) findViewById(R.id.addBtn);
+        addRouteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreen.this, RouteScreen.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void selectFragment(MenuItem item){
+
+        Intent newIntent = new Intent(this, this.getClass());
+        switch(item.getItemId()) {
+            case R.id.navigation_home:
+                newIntent = new Intent(this, HomeScreen.class);
+                break;
+            case R.id.navigation_routes:
+                newIntent = new Intent(this, RouteScreen.class);
+                startActivity(newIntent);
+                break;
+            case R.id.navigation_walk:
+                newIntent = new Intent(this, WalkScreen.class);
+                startActivity(newIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
