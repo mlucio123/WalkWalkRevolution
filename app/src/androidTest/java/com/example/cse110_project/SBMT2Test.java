b6e8f0da5cb8a2d97444761e2991adb2f4a76c72
Connor Prendiville <prendiville.connor@gmail.com>
2020-02-15 21:44:43 -0800

    issue #108: test done

--- app/src/androidTest/java/com/example/cse110_project/SBMT2Test.java
+++ app/src/androidTest/java/com/example/cse110_project/SBMT2Test.java
@@ -1,16 +1,12 @@
 package com.example.cse110_project;
 
 
-import android.content.SharedPreferences;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-import android.widget.LinearLayout;
 
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -20,9 +16,8 @@ import androidx.test.runner.AndroidJUnit4;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,7 +29,6 @@ import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -52,30 +46,23 @@ public class SBMT2Test {
     public GrantPermissionRule mGrantPermissionRule =
             GrantPermissionRule.grant(
                     "android.permission.ACCESS_FINE_LOCATION");
+
+    @BeforeClass
+    public static void setup() {
+
+    }
+
     @Before
     public void setSharedPrefs() {
+        HomeScreen.USE_GOOGLE_FIT_TESTER = true;
+        WalkScreen.USE_TEST_SERVICE = true;
         Log.d("SAVING", "PREFS");
         AccessSharedPrefs.setUserInfo(mActivityTestRule.getActivity(), "Connor",
                 "Prendiville", 6, 0);
     }
 
-    /*@After
-    public void clearSharedPrefs()
-    }*/
-
     @Test
-    public void sBMT2Test() {
-        Log.d("IN", "TEST");
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.homeTitle), withText("Home"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textView.check(matches(withText("Home")));
-
+    public void sBMT2() {
         ViewInteraction appCompatButton2 = onView(
                 allOf(withId(R.id.startBtn), withText("START WALK"),
                         childAtPosition(
@@ -86,7 +73,7 @@ public class SBMT2Test {
                         isDisplayed()));
         appCompatButton2.perform(click());
 
-        SystemClock.sleep(5000);
+        SystemClock.sleep(500);
 
         ViewInteraction appCompatButton3 = onView(
                 allOf(withId(R.id.startWalkMaterial), withText("Start Walking!"),
@@ -100,56 +87,6 @@ public class SBMT2Test {
         appCompatButton3.perform(click());
 
         ViewInteraction appCompatButton4 = onView(
-                allOf(withId(R.id.boostStepBtn), withText("ADD STEP"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.startStopContainer),
-                                        3),
-                                0),
-                        isDisplayed()));
-        appCompatButton4.perform(click());
-
-        ViewInteraction appCompatButton5 = onView(
-                allOf(withId(R.id.boostStepBtn), withText("ADD STEP"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.startStopContainer),
-                                        3),
-                                0),
-                        isDisplayed()));
-        appCompatButton5.perform(click());
-
-        ViewInteraction appCompatButton6 = onView(
-                allOf(withId(R.id.boostStepBtn), withText("ADD STEP"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.startStopContainer),
-                                        3),
-                                0),
-                        isDisplayed()));
-        appCompatButton6.perform(click());
-
-        ViewInteraction appCompatButton7 = onView(
-                allOf(withId(R.id.boostStepBtn), withText("ADD STEP"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.startStopContainer),
-                                        3),
-                                0),
-                        isDisplayed()));
-        appCompatButton7.perform(click());
-
-        ViewInteraction appCompatButton8 = onView(
-                allOf(withId(R.id.boostStepBtn), withText("ADD STEP"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.startStopContainer),
-                                        3),
-                                0),
-                        isDisplayed()));
-        appCompatButton8.perform(click());
-
-        ViewInteraction appCompatButton9 = onView(
                 allOf(withId(R.id.stopWalkMaterial), withText("End My Walk"),
                         childAtPosition(
                                 allOf(withId(R.id.startStopContainer),
@@ -158,17 +95,7 @@ public class SBMT2Test {
                                                 6)),
                                 0),
                         isDisplayed()));
-        appCompatButton9.perform(click());
-
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.newRouteTitle), withText("New Route"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(LinearLayout.class),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textView2.check(matches(withText("New Route")));
+        appCompatButton4.perform(click());
 
         ViewInteraction appCompatEditText5 = onView(
                 allOf(withId(R.id.routeName),
@@ -177,104 +104,35 @@ public class SBMT2Test {
                                         withClassName(is("android.widget.LinearLayout")),
                                         1),
                                 1)));
-        appCompatEditText5.perform(scrollTo(), click());
+        appCompatEditText5.perform(scrollTo(), replaceText("Espresso"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText6 = onView(
-                allOf(withId(R.id.routeName),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        1),
-                                1)));
-        appCompatEditText6.perform(scrollTo(), click());
-
-        ViewInteraction appCompatEditText7 = onView(
-                allOf(withId(R.id.routeName),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        1),
-                                1)));
-        appCompatEditText7.perform(scrollTo(), click());
-
-        ViewInteraction appCompatEditText8 = onView(
-                allOf(withId(R.id.routeName),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        1),
-                                1)));
-        appCompatEditText8.perform(scrollTo(), replaceText("Regular Walk"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText9 = onView(
                 allOf(withId(R.id.routeStart),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         2),
                                 1)));
-        appCompatEditText9.perform(scrollTo(), replaceText("Stressman and Bragg"), closeSoftKeyboard());
+        appCompatEditText6.perform(scrollTo(), replaceText("Esspresso"), closeSoftKeyboard());
 
-        ViewInteraction appCompatButton10 = onView(
+        ViewInteraction appCompatButton5 = onView(
                 allOf(withId(R.id.submitBtn), withText("Submit"),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         10),
                                 1)));
-        appCompatButton10.perform(scrollTo(), click());
+        appCompatButton5.perform(scrollTo(), click());
 
-        SystemClock.sleep(10000);
-
-        ViewInteraction button = onView(
-                allOf(withText("Expand"),
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.routeTitle), withText("Route"),
                         childAtPosition(
                                 childAtPosition(
-                                        withClassName(is("android.widget.LinearLayout")),
-                                        3),
+                                        withId(android.R.id.content),
+                                        0),
                                 0),
                         isDisplayed()));
-        button.perform(click());
-
-        ViewInteraction textView3 = onView(
-                allOf(withText("1.17 Miles"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(LinearLayout.class),
-                                        2),
-                                1),
-                        isDisplayed()));
-        textView3.check(matches(withText("1.17 Miles")));
-
-        ViewInteraction textView4 = onView(
-                allOf(withText("2500 Steps"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(LinearLayout.class),
-                                        3),
-                                1),
-                        isDisplayed()));
-        textView4.check(matches(withText("2500 Steps")));
-
-        ViewInteraction textView5 = onView(
-                allOf(withText("Regular Walk"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(LinearLayout.class),
-                                        0),
-                                1),
-                        isDisplayed()));
-        textView5.check(matches(withText("Regular Walk")));
-
-        ViewInteraction textView6 = onView(
-                allOf(withText("Stressman and Bragg"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(LinearLayout.class),
-                                        1),
-                                1),
-                        isDisplayed()));
-        textView6.check(matches(withText("Stressman and Bragg")));
+        textView.check(matches(withText("Route")));
     }
 
     private static Matcher<View> childAtPosition(
@@ -292,25 +150,6 @@ public class SBMT2Test {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
                         && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
-    public static ViewAction waitFor(final long millis) {
-        return new ViewAction() {
-            @Override
-            public Matcher<View> getConstraints() {
-                return isRoot();
-            }
-
-            @Override
-            public String getDescription() {
-                return "Wait for " + millis + " milliseconds.";
-            }
-
-            @Override
-            public void perform(UiController uiController, final View view) {
-                uiController.loopMainThreadForAtLeast(millis);
             }
         };
     }
