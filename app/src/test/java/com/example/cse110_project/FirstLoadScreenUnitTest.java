09052e2861b047999bf996188be14edff9e5237a
Howard Lin <houan.howard@gmail.com>
2020-02-16 12:32:00 -0800

    merge staging

--- app/src/test/java/com/example/cse110_project/FirstLoadScreenUnitTest.java
+++ app/src/test/java/com/example/cse110_project/FirstLoadScreenUnitTest.java
@@ -14,6 +14,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.android.internal.LocalPermissionGranter;
+
 import static android.content.Context.MODE_PRIVATE;
 import static org.junit.Assert.assertEquals;
 
