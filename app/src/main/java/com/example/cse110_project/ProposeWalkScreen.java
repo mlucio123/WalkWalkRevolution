d75e95d0fd0da1a6489e3478a2ebb04eefcc4796
ChufanYang <chy207@ucsd.edu>
2020-03-01 17:30:15 -0800

    add UI

---
+++ app/src/main/java/com/example/cse110_project/ProposeWalkScreen.java
@@ -0,0 +1,14 @@
+package com.example.cse110_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProposeWalkScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.propose_walk_screen);
+    }
+}
