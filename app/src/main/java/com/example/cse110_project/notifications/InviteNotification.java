60256dee2649df737236ca1627f4ac3aa340fc4f
Mia Lucio <mlucio@ucsd.edu>
2020-03-03 11:36:25 -0800

    notif stuff

--- app/src/main/java/com/example/cse110_project/notifications/InviteNotification.java
+++ app/src/main/java/com/example/cse110_project/notifications/InviteNotification.java
@@ -3,12 +3,14 @@ package com.example.cse110_project.notifications;
 public class InviteNotification implements Notification {
     private Notification.NotifType type;
     private String fromName;
+    private String teamID;
     private boolean accepted;
 
-    public InviteNotification( String fromName, boolean accepted) {
+    public InviteNotification( String fromName, String teamID,  boolean accepted) {
         this.type = NotifType.InviteNotification;
         this.fromName = fromName;
         this.accepted = accepted;
+        this.teamID = teamID;
     }
 
 
