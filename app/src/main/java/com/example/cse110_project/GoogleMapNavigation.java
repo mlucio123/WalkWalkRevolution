3d20db82aef44ccc800a10a13d257658b41b05f4
qdxuezf <qdxuezf@gmail.com>
2020-03-03 15:47:10 -0800

    map opens in both Route and WalkingScre

--- app/src/main/java/com/example/cse110_project/GoogleMapNavigation.java
+++ app/src/main/java/com/example/cse110_project/GoogleMapNavigation.java
@@ -2,38 +2,32 @@ package com.example.cse110_project;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class GoogleMapNavigation extends AppCompatActivity {
+public class GoogleMapNavigation {
 
     protected final String URL = "https://www.google.com/maps/search/?api=1&query=";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.route_screen);
+    protected TextView startDisplay;
 
-        String latitude = "47.5951518";
-        String longitude = "-122.3316393";
+    public GoogleMapNavigation(TextView startDisplay) {
+        this.startDisplay = startDisplay;
+    }
 
-        String Query = URL + latitude + "," + longitude;
+    public Intent getURL() {
 
+        final String URL = "https://www.google.com/maps/search/?api=1&query=";
+        String name = startDisplay.getText().toString();
+        name.replace(" ", "+");
+        String Query = URL +  name;
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Query));
-        //Alternative approach that Directly start Google Maps rather than browser
-        /*Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");*/
 
-        startActivity(browserIntent);
+        return browserIntent;
 
     }
 
-    protected String getGPSCoordinate(Object scheduleWalk) {
-        // TODO : need to Implement scheduleWalk and gain corresponding information from there
-        return null;
-    }
 
 
 }
