2e99af7b41d7eee2004d2801e67cae359305e3ca
Jining Song <jiningsmail@gmail.com>
2020-03-03 03:27:45 -0800

    linked team_screen and add_teamate_screen

--- app/src/main/java/com/example/cse110_project/AddTeamateScreen.java
+++ app/src/main/java/com/example/cse110_project/AddTeamateScreen.java
@@ -1,15 +1,11 @@
 package com.example.cse110_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
 
 import com.example.cse110_project.Firebase.RouteCollection;
 import com.example.cse110_project.Firebase.TeamCollection;
@@ -17,30 +13,21 @@ import com.example.cse110_project.Firebase.UserCollection;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class TeamScreen extends AppCompatActivity {
+public class AddTeamateScreen extends AppCompatActivity {
 
     private String fitnessServiceKey = "GOOGLE_FIT";
     private BottomNavigationView bottomNavigationView;
-    private Button addTeamateBtn;
 
     public static boolean testing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.team_screen);
+        setContentView(R.layout.add_teamate_screen);
 
         RouteCollection.initFirebase(this);
         UserCollection.initFirebase(this);
         TeamCollection.initFirebase(this);
-        addTeamateBtn = (Button) findViewById(R.id.addBtn);
-        Intent intent = new Intent(this, AddTeamateScreen.class);
-        addTeamateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intent);
-            }
-        });
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
