fe2c070c9487119dc1d2642f50d733771c9ed581
Mia Lucio <mlucio@ucsd.edu>
2020-03-07 17:20:27 -0800

    switching to teamNotificationUI

--- app/src/main/java/com/example/cse110_project/TeamNotificationScreen.java
+++ app/src/main/java/com/example/cse110_project/TeamNotificationScreen.java
@@ -1,4 +1,16 @@
 package com.example.cse110_project;
 
-public class TeamNotificationScreen {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeamNotificationScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.team_notification_screen);
+
+        
+
+    }
 }
