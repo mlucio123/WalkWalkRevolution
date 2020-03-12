c48ea155e461ef01cd480f9ebaf43bb8ad6f4cf4
Connor Prendiville <prendiville.connor@gmail.com>
2020-03-12 12:32:02 -0700

    9 passes

--- app/src/androidTest/java/com/example/cse110_project/SBMT9Test.java
+++ app/src/androidTest/java/com/example/cse110_project/SBMT9Test.java
@@ -179,23 +179,6 @@ public class SBMT9Test {
                         isDisplayed()));
         appCompatImageButton.perform(click());
 
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction button = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.notif_container),
-                                0),
-                        1),
-                        isDisplayed()));
-        button.check(matches(isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
